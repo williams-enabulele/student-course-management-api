e70871b915901940ec0d92dcab2719ab949ebcad
Enabulele Williams <enabsdigital@gmail.com>
2022-05-30 15:37:01 +0100

    updated dto

--- src/main/java/com/course/management/dto/StudentDTO.java
+++ src/main/java/com/course/management/dto/StudentDTO.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
